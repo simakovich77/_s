19d6740abb685423b168a4d2ff90fc70ac28e587
simakovich77 <simakovich77@gmail.com>
2017-04-17 16:25:37 +0300

    test_task Simakovich_I_A

--- functions.php
+++ functions.php
@@ -141,3 +141,181 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+//Add variable url with link to admin-ajax.php
+add_action( 'wp_enqueue_scripts', '_s_ajax_data', 99 );
+function _s_ajax_data(){
+    wp_localize_script('s-script', 's_customize_register_theme',
+        array(
+            'url' => admin_url('admin-ajax.php')
+        )
+    );
+}?>
+
+<?php
+//Add action for jQuery function
+add_action('wp_footer', '_s_action_javascript', 99);
+function _s_action_javascript() {
+    ?>
+    <script type="text/javascript" >
+        jQuery(document).ready(function($) {
+            var data = {
+                action: '_s_action'
+            };
+            //Request on url with name of action
+            jQuery.post( _s_ajax.url, data, function(response) {
+                //entry into div with id #posts response from server
+                $('#posts').html(response);
+            });
+        });
+    </script>
+    <?php
+}
+//Add callback function for user with administrator priveleges and without it
+add_action('wp_ajax__s_action', '_s_action_callback');
+add_action('wp_ajax_nopriv_s_action', '_s_action_callback');
+
+function _s_action_callback()
+{
+
+    //set custom criteria for select data from database
+    $args = array(
+        'posts_per_page' => 5,
+        'orderby' => 'comment_count'
+    );
+    $query = new WP_Query( $args );
+    if ( $query->have_posts() ) {
+        while ( $query->have_posts() ) {
+            $query->the_post();
+            echo '<a href="'.get_permalink().'">'.get_the_title().'"</a><br>';
+        }
+    } else {
+        echo 'Posts not found!';
+    }
+    //Return original data of posts
+    wp_reset_postdata();
+    wp_die();
+}
+
+//function for change color fonts and logo
+function s_customize_register( $wp_customize )
+{
+    //Description section for change color of fonts in site
+    $wp_customize->add_section( 'section_color_text' , array(
+        'title'      => __( 'Цвет шрифтов', 's' ),
+        'priority'   => 30,
+    ) );
+    //Description settings for change color of fonts in site
+    $wp_customize->add_setting( 'setting_color_text' , array(
+        'default'     => '#000000',
+        'transport'   => 'refresh',
+    ) );
+    //Description control for section_color_text and setting_color_text - for section and controller for change color of fonts in site
+    $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'link_color', array(
+        'label'      => __( 'Color', 's' ),
+        'section'    => 'section_color_text',
+        'settings'   => 'setting_color_text',
+    ) ) );
+    //Description section for change logo in main page
+    $wp_customize->add_section( 'section_logo_picture' , array(
+        'title'      => __( 'Картинка', 's' ),
+        'priority'   => 31,
+    ) );
+    //Description settings of section for change logo in main page
+    $wp_customize->add_setting( 'setting_logo_picture' , array(
+        'default'     => '#000000',
+        'transport'   => 'refresh',
+    ) );
+    //Description control for section_logo_picture и setting_logo_picture - for section and controller for change logo in main page
+    $wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'link_picture', array(
+        'label'      => __( 'Logo', 's' ),
+        'section'    => 'section_logo_picture',
+        'settings'   => 'setting_logo_picture',
+    ) ) );
+}
+//CSS settings for change color text
+add_action( 'customize_register', 's_customize_register');
+    function s_customize_css()
+    {
+        ?>
+        <style type="text/css">
+            body { color: <?php echo get_theme_mod('setting_color_text', '#000000'); ?>; }
+        </style>
+        <?php
+    }
+    add_action( 'wp_head', 's_customize_css');
+?>
+
+
+<?php
+    //Add custom meta box
+    function add_custom_meta_box()
+    {
+    add_meta_box("demo-meta-box", "Custom Meta Box", "custom_meta_box_markup", "post", "side", "high", null);
+    }
+    add_action("add_meta_boxes", "add_custom_meta_box");
+?>
+
+
+<?php
+//Creating custom meta box
+function custom_meta_box_markup($object)
+{
+    wp_nonce_field(basename(__FILE__), "meta-box-nonce");
+
+    ?>
+        <label for="meta-box-dropdown">Dropdown</label>
+        <select name="meta-box-dropdown">
+            <?php
+            //Select values
+            $option_values = array(1, 2, 3, 4, 5);
+
+            foreach($option_values as $key => $value)
+            {
+                if($value == get_post_meta($object->ID, "meta-box-dropdown", true))
+                {
+                    ?>
+                    <option selected><?php echo $value; ?></option>
+                    <?php
+                }
+                else
+                {
+                    ?>
+                    <option><?php echo $value; ?></option>
+                    <?php
+                }
+            }
+            ?>
+        </select>
+        <br>
+    </div>
+    <?php
+}
+?>
+
+<?php
+//Save data of meta box
+function save_custom_meta_box($post_id, $post, $update)
+{
+    if (!isset($_POST["meta-box-nonce"]) || !wp_verify_nonce($_POST["meta-box-nonce"], basename(__FILE__)))
+        return $post_id;
+
+    if (!current_user_can("edit_post", $post_id))
+        return $post_id;
+
+    if (defined("DOING_AUTOSAVE") && DOING_AUTOSAVE)
+        return $post_id;
+    $slug = "post";
+
+    if ($slug != $post->post_type)
+        return $post_id;
+    $meta_box_dropdown_value = "";
+
+    if (isset($_POST["meta-box-dropdown"])) {
+        $meta_box_dropdown_value = $_POST["meta-box-dropdown"];
+    }
+    update_post_meta($post_id, "meta-box-dropdown", $meta_box_dropdown_value);
+
+}
+add_action("save_post", "save_custom_meta_box", 10, 3);
+?>
--- header.php
+++ header.php
@@ -16,16 +16,29 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
+    <script src="<?php bloginfo('url');?>/wp-content/themes/_s/js/jquery-3.2.0.min.js"></script>
+
+    <script type='text/javascript'>
+        var _s_ajax = {
+            "url":"<?php bloginfo('url');?>/wp-admin/admin-ajax.php"
+        };
+    </script>
+
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
+<?php echo get_theme_mod('section_logo_picture', ''); ?>
+<img id="logo" src="<?php echo get_theme_mod('setting_logo_picture', ''); ?>" />
+
+
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', '_s' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
 			<?php
+
 			if ( is_front_page() && is_home() ) : ?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<?php else : ?>
@@ -40,10 +53,7 @@
 			endif; ?>
 		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', '_s' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
+
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
